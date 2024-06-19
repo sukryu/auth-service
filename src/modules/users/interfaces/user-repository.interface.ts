8346ef4c125f9f421e405276ff3b5cb982b417ac
sukryu <hjinhyeog162@gmail.com>
2024-06-20 00:22:50 +0900

    fixed user`s repository layer

--- src/modules/users/interfaces/user-repository.interface.ts
+++ src/modules/users/interfaces/user-repository.interface.ts
@@ -4,7 +4,7 @@ import { UserEntity } from "../entities/user.entity";
 
 export interface UserRepositoryInterface {
     create(createUserDto: CreateUserDto): Promise<UserEntity>;
-    update(userId: string, updateUserDto: UpdateUserDto): Promise<UserEntity>;
+    update(user: UserEntity): Promise<UserEntity>;
     delete(userId: string): Promise<void>;
 
     getUsers(): Promise<UserEntity[]>;
--- src/modules/users/repository/users.repository.ts
+++ src/modules/users/repository/users.repository.ts
@@ -1,30 +1,67 @@
-import { BadRequestException, Injectable, Logger, NotFoundException } from "@nestjs/common";
+import { Injectable, InternalServerErrorException, Logger, NotFoundException } from "@nestjs/common";
 import { UserRepositoryInterface } from "../interfaces/user-repository.interface";
 import { InjectRepository } from "@nestjs/typeorm";
 import { UserEntity } from "../entities/user.entity";
 import { Repository } from "typeorm";
 import { CreateUserDto } from "../dto/create-user.dto";
-import { UpdateUserDto } from "../dto/update-user.dto";
-import * as bcrypt from 'bcrypt';
 import { UtilsService } from "src/common/utils/utils";
+import { RedisService } from "src/modules/redis/redis.service";
+import { ConfigService } from "@nestjs/config";
+import { plainToClass } from "class-transformer";
 
 @Injectable()
 export class UsersRepository implements UserRepositoryInterface {
     private readonly logger = new Logger(UsersRepository.name);
+    private readonly TTL = this.configService.get<number>("REDIS_TTL");
     constructor(
         @InjectRepository(UserEntity)
         private readonly repository: Repository<UserEntity>,
         private readonly utils: UtilsService,
+        private readonly redis: RedisService,
+        private readonly configService: ConfigService,
     ) {}
+
+    public async setCacheUser(user: UserEntity): Promise<void> {
+        await this.utils.handleCommonErrors(user);
+        const hashKey = `user:${user.id}`;
+        await this.redis.set(hashKey, JSON.stringify(user), this.TTL);
+    }
+
+    public async getCacheUser(key: string): Promise<UserEntity> {
+        const cacheUser = await this.redis.get(key);
+        if (!cacheUser) {
+            this.logger.error(`Cannot find cache user: ${key}`);
+            throw new NotFoundException(`Cannot find cache user: ${key}`);
+        }
+
+        try {
+            const user = plainToClass(UserEntity, JSON.parse(cacheUser));
+            await this.utils.handleCommonErrors(user);
+            return user;
+        } catch (error) {
+            this.logger.error(`Failed to parse user data from cache: ${error.message}`);
+            throw new InternalServerErrorException(`Failed to parse user data from cache`);
+        }
+    }
 
     public async getUsers(): Promise<UserEntity[]> {
         return await this.repository.find();
     }
 
     public async getUserById(userId: string): Promise<UserEntity> {
-        const user = await this.repository.findOne({ where: { id: userId }});
-        await this.utils.handleCommonErrors(user);
-        return user;
+        const cacheKey = `user:${userId}`;
+        const cachedUser = await this.getCacheUser(cacheKey);
+        if (cachedUser) {
+            this.logger.debug(`Cache hit for user: ${userId}`);
+            await this.utils.handleCommonErrors(cachedUser);
+            return cachedUser;
+        } else {
+            this.logger.debug(`Cache miss for user: ${userId}`);
+            const user = await this.repository.findOne({ where: { id: userId }});
+            await this.utils.handleCommonErrors(user);
+            await this.setCacheUser(user);
+            return user;
+        }
     }
 
     public async getUserByEmail(email: string): Promise<UserEntity> {
@@ -39,15 +76,15 @@ export class UsersRepository implements UserRepositoryInterface {
         return await this.repository.save(user);
     }
 
-    public async update(userId: string, updateUserDto: UpdateUserDto): Promise<UserEntity> {
-        const user = await this.repository.findOne({ where: { id: userId }});
+    public async update(user: UserEntity): Promise<UserEntity> {
         await this.utils.handleCommonErrors(user);
 
-        if (updateUserDto.email) user.email = updateUserDto.email;
-        if (updateUserDto.password) user.password = await this.hashPassword(updateUserDto.password);
-        if (updateUserDto.username) user.username = updateUserDto.username;
+        const cacheKey = `user:${user.id}`;
+        await this.redis.del(cacheKey);
 
-        return await this.repository.save(user);
+        const updateUser = await this.repository.save(user);
+        await this.setCacheUser(updateUser);
+        return updateUser;
     }
 
     public async delete(userId: string): Promise<void> {
@@ -55,9 +92,5 @@ export class UsersRepository implements UserRepositoryInterface {
         await this.utils.handleCommonErrors(user);
 
         await this.repository.softDelete(userId);
-    }
-
-    private async hashPassword(password: string): Promise<string> {
-        return await bcrypt.hash(password, 10);
     }
 }
